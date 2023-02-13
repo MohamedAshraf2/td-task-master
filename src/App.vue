b8e77a8b71da681a5884907c15e29f98ccd31983
MohamedAshraf2 <mohamashraf144@gmail.com>
2023-02-13 16:01:26 +0200

    second commit adding SASS

--- src/App.vue
+++ src/App.vue
@@ -57,50 +57,41 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-.header {
-  display: flex; 
-  align-self: start;
-  font-size: 30px;
-  font-weight: bold; 
-  position: relative;
-  color: #3a3a3a;
-}
-
-.header:after {
-  content: '';
-  position: absolute;
-  width: 100%;
-  transform: scaleX(0);
-  height: 2px;
-  bottom: 0;
-  left: 0;
-  background-color: #c7ca00;
-  transform-origin: bottom right;
-  transition: transform 1s ease-out;
-}
-.header:hover:after {
-  transform: scaleX(1);
-  transform-origin: bottom left;
-}
-
-.btnContainer {
-  display: flex;
-  margin: 3%;
-  justify-content: center;
-}
-
-button{
-  background-color: white;
-  color: black;
-}
+<style lang="sass">
+#app 
+	font-family: Avenir, Helvetica, Arial, sans-serif 
+	-webkit-font-smoothing: antialiased 
+	-moz-osx-font-smoothing: grayscale 
+	text-align: center 
+	color: #2c3e50 
+	margin-top: 60px 
+.header 
+	display: flex 
+	align-self: start 
+	font-size: 30px 
+	font-weight: bold 
+	position: relative 
+	color: #3a3a3a 
+	&:after 
+		content: '' 
+		position: absolute 
+		width: 100% 
+		transform: scaleX(0) 
+		height: 2px 
+		bottom: 0 
+		left: 0 
+		background-color: #c7ca00 
+		transform-origin: bottom right 
+		transition: transform 1s ease-out 
+	&:hover 
+		&:after 
+			transform: scaleX(1) 
+			transform-origin: bottom left 
+.btnContainer 
+	display: flex 
+	margin: 3% 
+	justify-content: center 
+button 
+	background-color: white 
+	color: black 
 </style>
--- src/components/OneItem.vue
+++ src/components/OneItem.vue
@@ -38,61 +38,47 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="sass">
 .v-enter-active,
-.v-leave-active {
-  transition: opacity 2s ease, transform 1s ease-in-out;
-  transform: translateY(0px);
-}
+.v-leave-active 
+	transition: opacity 2s ease, transform 1s ease-in-out 
+	transform: translateY(0px) 
 .v-enter-from,
-.v-leave-to {
-  opacity: 0;
-  transform: translateY(150px);
-}
-.ImgComponent {
-  margin-top: 10px;
-  height: 210px;
-  background-size: cover;
-  display: flex;
-  align-items: flex-end;
-  position: relative;
-}
-.overlay {
-  width: 100%;
-  height: 210px;
-  z-index: 2;
-  position: absolute;
-  background: -webkit-gradient(
-    linear,
-    left top,
-    left bottom,
-    color-stop(0%, rgba(137, 255, 241, 0)),
-    color-stop(100%, rgba(0, 0, 0, 1))
-  );
-}
-.content {
-  z-index: 3;
-}
-.title {
-  color: white;
-  font-size: 20px;
-  font-weight: bold;
-  margin: 2%;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  display: -webkit-box;
-  text-align: start;
-  -webkit-line-clamp: 1;
-  -webkit-box-orient: vertical;
-}
-.title:hover {
-  -webkit-box-orient: horizontal;
-}
-.date {
-  color: rgb(179, 179, 179);
-  font-size: 13px;
-  margin: 0% 0% 3% 3%;
-  align-self: start;
-  display: flex;
-}
+.v-leave-to 
+	opacity: 0 
+	transform: translateY(150px) 
+.ImgComponent 
+	margin-top: 10px 
+	height: 210px 
+	background-size: cover 
+	display: flex 
+	align-items: flex-end 
+	position: relative 
+.overlay 
+	width: 100% 
+	height: 210px 
+	z-index: 2 
+	position: absolute 
+	background: -webkit-gradient(linear,left top,left bottom,color-stop(0%, rgba(137, 255, 241, 0)),color-stop(100%, rgba(0, 0, 0, 1))) 
+.content 
+	z-index: 3 
+.title 
+	color: white 
+	font-size: 20px 
+	font-weight: bold 
+	margin: 2% 
+	text-overflow: ellipsis 
+	overflow: hidden 
+	display: -webkit-box 
+	text-align: start 
+	-webkit-line-clamp: 1 
+	-webkit-box-orient: vertical 
+	&:hover 
+		-webkit-box-orient: horizontal 
+.date 
+	color: rgb(179, 179, 179) 
+	font-size: 13px 
+	margin: 0% 0% 3% 3% 
+	align-self: start 
+	display: flex 
 </style>
